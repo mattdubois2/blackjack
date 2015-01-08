c1026d1f447297c7f704b85da888ee252cb31bb1
Matt DuBois <mattdubois2@gmail.com>
2015-01-08 14:19:45 -0600

    set up migrations, models, and seeds

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+
+suits = %w(hearts spades diamonds clubs)
+names = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+suits.each do |suit|
+  names.each_with_index do |name, i|
+    Card.create!(suit: suit, name: name, value: values[i])
+  end
+end
